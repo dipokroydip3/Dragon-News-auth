a54c79c66ba16a20f616157317b8ba6c84f8e39a
dipokroydip3 <dipokroydip3>
2023-10-03 01:20:59 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXbdT4RXz_LvSf8WZ2Lgvb6g3Cucpg-iU",
+  authDomain: "dragon-news-auth-e81d5.firebaseapp.com",
+  projectId: "dragon-news-auth-e81d5",
+  storageBucket: "dragon-news-auth-e81d5.appspot.com",
+  messagingSenderId: "129691923103",
+  appId: "1:129691923103:web:f542da3e46e66f6acee8aa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
